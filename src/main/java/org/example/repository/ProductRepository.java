f50176e687da9cee0c5e2b399573f3a749c23899
Xarlie13 <cmbvgalvao@users.noreply.github.com>
2023-12-30 18:23:26 +0000

    Delete src/main directory

--- src/main/java/org/example/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
