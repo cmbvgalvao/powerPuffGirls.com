c6e0aa45db71e3a9b446701b832e2696335cafde
Xarlie13 <cmbvgalvao@users.noreply.github.com>
2023-12-30 18:23:17 +0000

    Delete target directory

--- target/classes/db/test-data.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO Product(item_name, items_in_stock, price) VALUES ('T-Shirt', 20, 24.99);
-INSERT INTO Product(item_name, items_in_stock, price) VALUES ('Mug', 20, 9.99);
-INSERT INTO Product(item_name, items_in_stock, price) VALUES ('Key_chain', 20, 4.99);
